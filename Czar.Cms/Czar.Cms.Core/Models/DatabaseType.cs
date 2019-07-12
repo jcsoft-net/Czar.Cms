85330b5af9b247a01f0ba43dd66ccdc0f345496e
zjfwjm1987 <860193679@qq.com>
2019-07-12 11:05:01 +0800

    数据库工厂

---
+++ Czar.Cms/Czar.Cms.Core/Models/DatabaseType.cs
@@ -0,0 +1,20 @@
+﻿namespace Czar.Cms.Core.Models
+{
+    /// <summary>
+    /// 数据库类型
+    /// </summary>
+    public enum DatabaseType
+    {
+        SqlServer,
+        MySQL,
+        PostgreSQL,
+        SQLite,
+        InMemory,
+        Oracle,
+        MariaDB,
+        MyCat,
+        Firebird,
+        DB2,
+        Access
+    }
+}
