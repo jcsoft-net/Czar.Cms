332dc6da63bacae00a6812cf5758b4fc7a928d18
zjfwjm1987 <860193679@qq.com>
2019-07-15 17:26:51 +0800

    layui

---
+++ Czar.Cms/Czar.Cms.Core/Options/DbOption.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// 数据库连接选项
+/// </summary>
+namespace Czar.Cms.Core.Options
+{
+    public class DbOption
+    {
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        public string ConnectionString { get; set; }
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public string DbType { get; set; }
+    }
+}
---
+++ Czar.Cms/Czar.Cms.Core/Repository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Czar.Cms.Core.Repository
+{
+    class IUnitOfWork
+    {
+    }
+}
