817f34e8ea23b8b2dafff8376e0ba0fce6159d0e
zjfwjm1987 <860193679@qq.com>
2019-07-22 17:46:30 +0800

    完善web 开发

---
+++ Czar.Cms/Czar.Cms.Admin/Filter/GlobalExceptionFilter.cs
@@ -0,0 +1,32 @@
+﻿using Czar.Cms.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLog;
+using System.Net;
+
+namespace Czar.Cms.Admin.Filter
+{
+    /// <summary>
+    /// 全局异常过滤k
+    /// </summary>
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void OnException(ExceptionContext context)
+        {
+            logger.Error(context.Exception);
+
+
+            var result = new BaseResult()
+            {
+                ResultCode = ResultCodeAddMsgKeys.CommonExceptionCode,//系统异常代码
+                ResultMsg = ResultCodeAddMsgKeys.CommonExceptionMsg,//系统异常信息
+            };
+
+            context.Result = new ObjectResult(result);
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ Czar.Cms/Czar.Cms.Admin/Validation/ManagerLockStatusModelValidation.cs
@@ -0,0 +1,17 @@
+﻿using Czar.Cms.ViewModels;
+using FluentValidation;
+
+namespace Czar.Cms.Admin.Validation
+{
+    public class ManagerLockStatusModelValidation : AbstractValidator<ChangeStatusModel>
+    {
+        public ManagerLockStatusModelValidation()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(x => x.Id).NotNull().GreaterThan(0).WithMessage("主键不能为空");
+
+            RuleFor(x => x.Status).NotNull().WithMessage("状态不能为空") ;
+        }
+    }
+}
